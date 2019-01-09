aa9cb5ce2a3afc532c4e20fa450014f83f223139
xiaojie zhang <xiaojiezhang@S0106441c120f48e6.ed.shawcable.net>
2019-01-09 14:24:27 -0500

    fix bug

--- assets/scripts/user2/ui.js
+++ assets/scripts/user2/ui.js
@@ -2,7 +2,7 @@
  * @Author: xiaojiezhang
  * @Date:   2019-01-04T19:57:56-05:00
  * @Last modified by:   xiaojiezhang
- * @Last modified time: 2019-01-09T13:59:12-05:00
+ * @Last modified time: 2019-01-09T14:24:02-05:00
  */
 const store = require('../store')
 const help = require('../help')
